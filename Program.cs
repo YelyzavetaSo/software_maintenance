b9e0bf0e2978186ab6323a139efe4216d1a47817
YelyzavetaSo <77263087+YelyzavetaSo@users.noreply.github.com>
2022-09-24 19:50:16 +0300

    update: print what have to be entered

--- Program.cs
+++ Program.cs
@@ -6,13 +6,15 @@ namespace Project
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введiть значення А:");
             int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введiть значення B:");
             int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введiть значення модуля Mod:");
             int mod = int.Parse(Console.ReadLine());
             int result = (a + b) % mod;
-            Console.WriteLine(string.Format("{0} + {1} mod {2} = {3}", a, b, mod, result));
+            Console.WriteLine(string.Format("Результат додавання:\n{0} + {1} mod {2} = {3}", a, b, mod, result));
             Console.ReadLine();
-
         }
     }
 }
