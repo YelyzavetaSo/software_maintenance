95e3216ef463b5eaffaf63d800798e40c1365f2c
YelyzavetaSo <77263087+YelyzavetaSo@users.noreply.github.com>
2022-09-24 19:30:38 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int mod = int.Parse(Console.ReadLine());
+            int result = (a + b) % mod;
+            Console.WriteLine(string.Format("{0} + {1} mod {2} = {3}", a, b, mod, result));
+            Console.ReadLine();
+
+        }
+    }
+}
